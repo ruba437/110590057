764d22b3d1fe40ee6a654cf202bdd9ab1dfccaef
ruba437 <114576056+ruba437@users.noreply.github.com>
2022-11-04 23:46:48 +0800

    lab5

---
+++ LAB05/script.js
@@ -0,0 +1,73 @@
+//write your script here
+
+function px200() {
+  document.getElementById("table").style.width="200px";
+}
+function px500() {
+  document.getElementById("table").style.width="500px";
+}
+function px1() {
+  document.getElementById("table").style.borderSpacing = "1px";
+  document.getElementById("table").style.borderWidth = "1px";
+}
+function px10() {
+  document.getElementById("table").style.borderSpacing= "10px";
+  document.getElementById("table").style.borderWidth = "10px";
+}
+function px20() {
+  document.getElementById("table").style.borderSpacing= "20px";
+  document.getElementById("table").style.borderWidth = "20px";
+}
+
+function green() {
+  document.getElementById("table").style.backgroundColor = "lightgreen";
+  document.getElementById("cell1").style.backgroundColor = "lightgreen";
+  document.getElementById("cell2").style.backgroundColor = "lightgreen";
+  document.getElementById("cell3").style.backgroundColor = "lightgreen";
+  document.getElementById("cell4").style.backgroundColor = "lightgreen";
+  document.getElementById("cell5").style.backgroundColor = "lightgreen";
+  document.getElementById("cell6").style.backgroundColor = "lightgreen";
+}
+
+function blue() {
+  document.getElementById("table").style.backgroundColor = "lightblue";
+  document.getElementById("cell1").style.backgroundColor = "lightblue";
+  document.getElementById("cell2").style.backgroundColor = "lightblue";
+  document.getElementById("cell3").style.backgroundColor = "lightblue";
+  document.getElementById("cell4").style.backgroundColor = "lightblue";
+  document.getElementById("cell5").style.backgroundColor = "lightblue";
+  document.getElementById("cell6").style.backgroundColor = "lightblue";
+}
+
+function purple() {
+  document.getElementById("table").style.backgroundColor = "purple";
+  document.getElementById("cell1").style.backgroundColor = "purple";
+  document.getElementById("cell2").style.backgroundColor = "purple";
+  document.getElementById("cell3").style.backgroundColor = "purple";
+  document.getElementById("cell4").style.backgroundColor = "purple";
+  document.getElementById("cell5").style.backgroundColor = "purple";
+  document.getElementById("cell6").style.backgroundColor = "purple";
+}
+
+function orange() {
+  document.getElementById("table").style.backgroundColor = "orange";
+  document.getElementById("cell1").style.backgroundColor = "orange";
+  document.getElementById("cell2").style.backgroundColor = "orange";
+  document.getElementById("cell3").style.backgroundColor = "orange";
+  document.getElementById("cell4").style.backgroundColor = "orange";
+  document.getElementById("cell5").style.backgroundColor = "orange";
+  document.getElementById("cell6").style.backgroundColor = "orange";
+}
+
+function reset() {
+  document.getElementById("table").style.borderSpacing= "1px";
+  document.getElementById("table").style.borderWidth = "1px";
+  document.getElementById("table").style.width="200px";
+  document.getElementById("table").style.backgroundColor = "#EAE671";
+  document.getElementById("cell1").style.backgroundColor = "#EAE671";
+  document.getElementById("cell2").style.backgroundColor = "#EAE671";
+  document.getElementById("cell3").style.backgroundColor = "#EAE671";
+  document.getElementById("cell4").style.backgroundColor = "#EAE671";
+  document.getElementById("cell5").style.backgroundColor = "#EAE671";
+  document.getElementById("cell6").style.backgroundColor = "#EAE671";
+}
